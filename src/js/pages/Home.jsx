42240b99e111f803336c6cc54c24c9c754e85b24
Kelvin Ruiz <kelvinruiz.developer@gmail.com>
2023-05-08 22:50:08 -0800

    prototipo de tarjeta creado

--- src/js/pages/Home.jsx
+++ src/js/pages/Home.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Home(){
+    //Lista de usuarios
+    const [allUsers, setAllUsers] = useState([
+        {
+            fullname: "Kelvin Ruiz",
+            description: "Comenzado en la programacion",
+            email: "kelvinruizdev@gmail.com",
+            github: "kelvinruizdev"
+        }
+    ]);
+    
+    //Usuario actual
+    const [user, setUser] = useState({
+        fullname: "",
+        description: "",
+        email: "",
+        github: ""
+    });
+    
+
     return(
         <>
             
@@ -8,7 +27,29 @@ function Home(){
                 <div className="row justify-content-center">
                     <div className="col-12 col-md-6">
 
-                        <form className="border border-secondary rounded p-3">
+                        {/*Mapear los usuarios*/}
+                        {
+                            allUsers.map((item, index)=>{
+                                return (
+                                    <div className="personal-card border border-secondary rounded ">
+                                        <div>
+                                            <img src="https://avatars.githubusercontent.com/u/91167188?v=4" alt="Github Avatar"/>
+                                        </div>
+                                        <div>
+                                            <p>Fullname</p>
+                                            <p>Email</p>
+                                            <p>Github</p>
+                                            <p>Description</p>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+
+                        <form className="border border-secondary rounded p-3 mt-3">
+
+                            <h2 className="text-center">Fill in to create profile card</h2>
+
                             <div className="form-group">
                                 <label className="form-label" htmlFor="fullname">Fullname:</label>
                                 <input className="form-control" id="fullname" type="text"></input>
@@ -30,7 +71,7 @@ function Home(){
                             </div>    
 
                             <div className="text-center pt-3">
-                                <button className="btn btn-primary w-50" type="submit">Save</button>
+                                <button className="btn btn-primary w-50" type="submit">Create</button>
                             </div>
 
                         </form>  
